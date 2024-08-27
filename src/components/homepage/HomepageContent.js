1a8df11acd1a9984cf5cbd1ab365476de8ffad70
Kyle Recktenwald <kreck30590@gmail.com>
2024-08-27 15:04:26 -0400

    Add Keycloak Login to Button

--- src/components/homepage/HomepageContent.js
+++ src/components/homepage/HomepageContent.js
@@ -1,13 +1,18 @@
 import classes from "./HomepageContent.module.css";
+import AuthService from "../../services/AuthService"; // Adjust the path accordingly
 
 function HomepageContent({}) {
+  const handleLoginSignup = () => {
+    AuthService.doLogin(); // This will redirect the user to the Keycloak login screen
+  };
+
   return (
     <div className={classes.content}>
       <h1>Welcome to Runner Utils</h1>
       <p>Record your workout data and manage your running routes with ease.</p>
-      <a href="./runs/runs.html">
-        <button className={classes.startButton}>Get Started</button>
-      </a>
+      <button className={classes.startButton} onClick={handleLoginSignup}>
+        Login or Signup
+      </button>
     </div>
   );
 }
