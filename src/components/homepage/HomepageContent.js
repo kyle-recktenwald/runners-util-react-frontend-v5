ed24abdbbc141a9f74440f3cda43b417fffb89aa
Kyle Recktenwald <kreck30590@gmail.com>
2024-08-27 15:11:10 -0400

    Homepage Buttons

--- src/components/homepage/HomepageContent.js
+++ src/components/homepage/HomepageContent.js
@@ -1,18 +1,29 @@
+import { Link } from "react-router-dom";
+
 import classes from "./HomepageContent.module.css";
-import AuthService from "../../services/AuthService"; // Adjust the path accordingly
+import AuthService from "../../services/AuthService";
 
 function HomepageContent({}) {
+  const isLoggedIn = AuthService.isLoggedIn;
+
   const handleLoginSignup = () => {
-    AuthService.doLogin(); // This will redirect the user to the Keycloak login screen
+    AuthService.doLogin();
   };
 
   return (
     <div className={classes.content}>
       <h1>Welcome to Runner Utils</h1>
       <p>Record your workout data and manage your running routes with ease.</p>
-      <button className={classes.startButton} onClick={handleLoginSignup}>
-        Login or Signup
-      </button>
+      {!isLoggedIn && (
+        <button className={classes.homeButton} onClick={handleLoginSignup}>
+          Login or Signup
+        </button>
+      )}
+      {isLoggedIn && (
+        <Link to="/runs">
+          <button className={classes.homeButton}>Get Started</button>
+        </Link>
+      )}
     </div>
   );
 }
