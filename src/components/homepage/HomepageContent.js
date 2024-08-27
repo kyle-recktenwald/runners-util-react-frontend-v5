ea4f9cb2d0a1262bfebdfc02294eda09b3fb8e5d
Kyle Recktenwald <kreck30590@gmail.com>
2024-08-27 14:54:11 -0400

    Start Button CSS

--- src/components/homepage/HomepageContent.js
+++ src/components/homepage/HomepageContent.js
@@ -3,10 +3,10 @@ import classes from "./HomepageContent.module.css";
 function HomepageContent({}) {
   return (
     <div className={classes.content}>
-      <h1>Welcome to Runner's Utils</h1>
+      <h1>Welcome to Runner Utils</h1>
       <p>Record your workout data and manage your running routes with ease.</p>
       <a href="./runs/runs.html">
-        <button class="start-button">Get Started</button>
+        <button className={classes.startButton}>Get Started</button>
       </a>
     </div>
   );
